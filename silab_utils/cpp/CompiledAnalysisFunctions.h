17d30fbc0f1b6e585134e85ff7d570b3fcc89e02
DavidLP <david-leon.pohl@rub.de>
2016-04-13 10:05:54 +0200

    ENH: fast histogramming in C++

---
+++ silab_utils/cpp/CompiledAnalysisFunctions.h
@@ -0,0 +1,57 @@
+// This file provides fast analysis functions written in c++. This file is needed to circumvent some python limitations where
+// no sufficient pythonic solution is available.
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <ctime>
+#include <cmath>
+#include <exception>
+#include <algorithm>
+#include <sstream>
+
+
+// fast 1d index histogramming (bin size = 1, values starting from 0)
+void histogram_1d(int*& x, const unsigned int& rSize, const unsigned int& rNbinsX, uint32_t*& rResult)
+{
+	for (unsigned int i = 0; i < rSize; ++i){
+		if ((unsigned int) x[i] >= rNbinsX)
+			throw std::out_of_range("The histogram indices are out of range");
+		if (rResult[x[i]] < 4294967295)
+			++rResult[x[i]];
+		else
+			throw std::out_of_range("The histogram has more than 4294967295 entries per bin. This is not supported.");
+	}
+}
+
+
+// fast 2d index histogramming (bin size = 1, values starting from 0)
+void histogram_2d(int*& x, int*& y, const unsigned int& rSize, const unsigned int& rNbinsX, const unsigned int& rNbinsY, uint32_t*& rResult)
+{
+	for (unsigned int i = 0; i < rSize; ++i){
+		if ((unsigned int) x[i] >= rNbinsX || (unsigned int) y[i] >= rNbinsY)
+			throw std::out_of_range("The histogram indices are out of range");
+		if (rResult[x[i] * rNbinsY + y[i]] < 4294967295)
+			++rResult[x[i] * rNbinsY + y[i]];
+		else
+			throw std::out_of_range("The histogram has more than 4294967295 entries per bin. This is not supported.");
+	}
+}
+
+// fast 3d index histogramming (bin size = 1, values starting from 0)
+void histogram_3d(int*& x, int*& y, int*& z, const unsigned int& rSize, const unsigned int& rNbinsX, const unsigned int& rNbinsY, const unsigned int& rNbinsZ, uint32_t*& rResult)
+{
+	for (unsigned int i = 0; i < rSize; ++i){
+		if ((unsigned int) x[i] >= rNbinsX || (unsigned int) y[i] >= rNbinsY || (unsigned int) z[i] >= rNbinsZ){
+			std::stringstream errorString;
+			errorString<<"The histogram indices (x/y/z)=("<<x[i]<<"/"<<y[i]<<"/"<<z[i]<<") are out of range.";
+			throw std::out_of_range(errorString.str());
+		}
+		if (rResult[x[i] * rNbinsY * rNbinsZ + y[i] * rNbinsZ + z[i]] < 4294967295)
+			++rResult[x[i] * rNbinsY * rNbinsZ + y[i] * rNbinsZ + z[i]];
+		else
+			throw std::out_of_range("The histogram has more than 4294967295 entries per bin. This is not supported.");
+	}
+}
+
+
